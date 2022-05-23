f75f16ee5ef32fabf073221c79489c45c7bf5e31
Matthias Funke <matthias.funke@alation.com>
2022-05-23 08:07:05 +0100

    Moved 2 files to indexer dir

--- generateTypes.js
+++ generateTypes.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const definitions = require("@interlay/interbtc-types").default
 const types = definitions.types[0].types
 
-writeJson('./types.json', types);
-writeJson('./typesBundle.json', {types})
+writeJson('./indexer/types.json', types);
+writeJson('./indexer/typesBundle.json', {types})
 
 function writeJson(file, obj) {
     fs.writeFileSync(file, JSON.stringify(obj, null, 2));
